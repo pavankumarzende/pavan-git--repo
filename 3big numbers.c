40bdab2698cf432a61b52aa23d99891628161e17
pavankumarzende <97588219+pavankumarzende@users.noreply.github.com>
2022-01-12 15:55:23 +0530

    Add files via upload

---
+++ 3big numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int num1,num2,num3;
+	printf("Enter 3 numbers : ");
+	scanf("%d %d %d",&num1,&num2,&num3);
+if (num1>num2)	
+printf("num 1 is gretare than num2 ",num1);
+else
+printf("num 2 is gretare than num1 ",num2);
+else if(num1>num3)
+printf("num 1 is gretare than num3 ",num1);
+else if 
+printf("num 3 is gretare than num1 ",num3);
+else 
+printf("num2 is a big number ",num2);
+
+
+}
